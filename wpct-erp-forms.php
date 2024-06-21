8b15d2ce3326cf66361366b12027cc5be3a520ab
Lucas García <lucas@codeccoop.org>
2024-06-21 09:00:58 +0200

    feat: version 1.3.0

--- wpct-erp-forms.php
+++ wpct-erp-forms.php
@@ -8,7 +8,7 @@ Author:          Còdec
 Author URI:      https://www.codeccoop.org
 Text Domain:     wpct-erp-forms
 Domain Path:     /languages
-Version:         1.2.1
+Version:         1.3.0
 */
 
 namespace WPCT_ERP_FORMS;
@@ -20,7 +20,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('WPCT_ERP_FORMS_VERSION', '1.2.1');
+define('WPCT_ERP_FORMS_VERSION', '1.3.0');
 
 require_once 'abstract/class-singleton.php';
 require_once 'abstract/class-plugin.php';
