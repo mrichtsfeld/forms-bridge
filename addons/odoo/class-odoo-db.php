475b8a74788a691628febfbb27743acef8b3c6cf
Lucas García <lucas@codeccoop.org>
2024-12-13 03:05:30 +0100

    feat: split odoo addon in class based files

---
+++ addons/odoo/class-odoo-db.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace FORMS_BRIDGE;
+
+if (!defined('ABSPATH')) {
+    exit();
+}
+
+class Odoo_DB
+{
+    private $data = null;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    public function __get($name)
+    {
+        switch ($name) {
+            case 'backend':
+                return $this->backend();
+            case 'form_hooks':
+                return $this->form_hooks();
+            default:
+                return isset($this->data[$name]) ? $this->data[$name] : null;
+        }
+    }
+
+    private function backend()
+    {
+        return apply_filters(
+            'http_bridge_backend',
+            null,
+            $this->data['backend']
+        );
+    }
+
+    private function form_hooks()
+    {
+        $form_hooks = apply_filter('forms_bridge_form_hooks', []);
+        return array_values(
+            array_filter($form_hooks, function ($form_hook) {
+                return $form_hook->api === 'odoo' &&
+                    ($form_hook->database = $this->data['name']);
+            })
+        );
+    }
+}
---
+++ addons/odoo/class-odoo-form-hook.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace FORMS_BRIDGE;
+
+if (!defined('ABSPATH')) {
+    exit();
+}
+
+class Odoo_Form_Hook extends Form_Hook
+{
+    public function __get($name)
+    {
+        switch ($name) {
+            case 'api':
+                return 'odoo';
+            case 'method':
+                return 'POST';
+            case 'backend':
+                return $this->backend();
+            case 'database':
+                return $this->database();
+            default:
+                return parent::__get($name);
+        }
+    }
+
+    private function backend()
+    {
+        return $this->database()->backend;
+    }
+
+    private function database()
+    {
+        $dbs = Settings::get_setting('forms-bridge', 'odoo-api')->databases;
+        foreach ($dbs as $db) {
+            if ($db['name'] === $this->data['database']) {
+                return new Odoo_DB($db);
+            }
+        }
+    }
+}
