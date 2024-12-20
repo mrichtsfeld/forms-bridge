6f41b140d91c1ec4dbc0c59025f3472f6f4a0635
Lucas García <lucas@codeccoop.org>
2024-12-20 10:14:30 +0100

    feat: update version

--- forms-bridge.php
+++ forms-bridge.php
@@ -8,7 +8,7 @@ Author:          Còdec
 Author URI:      https://www.codeccoop.org
 Text Domain:     forms-bridge
 Domain Path:     /languages
-Version:         2.0.3
+Version:         2.0.4
 */
 
 namespace FORMS_BRIDGE;
