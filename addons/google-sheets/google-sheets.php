17a1f054e763a2b8d852a123bbcfa3ce1aa04869
Lucas García <lucas@codeccoop.org>
2024-12-21 04:06:31 +0100

    feat: conditionally load composer autoload

--- addons/google-sheets/google-sheets.php
+++ addons/google-sheets/google-sheets.php
@@ -8,7 +8,10 @@ if (!defined('ABSPATH')) {
     exit();
 }
 
-if (!class_exists('\POSTS_BRIDGE\Posts_Bridge')) {
+if (
+    !class_exists('\POSTS_BRIDGE\Posts_Bridge') &&
+    !class_exists('\Google\Client')
+) {
     require_once 'vendor/autoload.php';
 }
 
