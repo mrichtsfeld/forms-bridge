63cc8f1deb0486f45628fcbf16b97f980f7411cb
Lucas García <lucas@codeccoop.org>
2025-01-23 23:05:54 +0100

    feat: update version

--- forms-bridge.php
+++ forms-bridge.php
@@ -10,7 +10,7 @@
  * License URI:         http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:         forms-bridge
  * Domain Path:         /languages
- * Version:             2.3.0
+ * Version:             2.3.1
  * Requires PHP:        8.0
  * Requires at least:   6.7
  */
