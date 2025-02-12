89a1245752290e6b0306bfd4f33cdff5b5061ca8
Lucas García <lucas@codeccoop.org>
2025-02-12 05:03:11 +0100

    feat: brevo and mailchimp templates

---
+++ addons/rest-api/templates/brevo-contacts.php
@@ -0,0 +1,132 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit();
+}
+
+add_filter(
+    'forms_bridge_payload',
+    function ($payload, $hook) {
+        if ($hook->template !== 'brevo-contacts') {
+            return $payload;
+        }
+
+        $with_attributes = ['attriubtes' => []];
+
+        foreach ($payload as $field => $value) {
+            if ($field === 'email') {
+                $with_attributes[$field] = $value;
+            } elseif ($field === 'listIds' && $value) {
+                $with_attributes[$field] = array_map(
+                    'intval',
+                    explode(',', $value)
+                );
+            } else {
+                $with_attributes['attriubtes'][strtoupper($field)] = $value;
+            }
+        }
+
+        return $with_attributes;
+    },
+    9,
+    2
+);
+
+return [
+    'title' => __('Brevo Contacts', 'forms-bridge'),
+    'fields' => [
+        [
+            'ref' => '#backend',
+            'name' => 'base_url',
+            'label' => __('Brevo API URL', 'forms-bridge'),
+            'type' => 'string',
+            'value' => 'https://api.brevo.com/v3/',
+        ],
+        [
+            'ref' => '#backend',
+            'name' => 'name',
+            'label' => __(
+                'Label of the Brevo API backend connection',
+                'forms-bridge'
+            ),
+            'type' => 'string',
+            'default' => 'Brevo API',
+        ],
+        [
+            'ref' => '#hook',
+            'name' => 'method',
+            'label' => __('Bridge HTTP method', 'forms-bridge'),
+            'type' => 'string',
+            'value' => 'POST',
+        ],
+        [
+            'ref' => '#hook',
+            'name' => 'endpoint',
+            'label' => __('Bridge endpoint', 'forms-bridge'),
+            'type' => 'string',
+            'value' => '/v3/contacts',
+        ],
+        [
+            'ref' => '#backend/headers[]',
+            'name' => 'api-key',
+            'label' => __('Brevo API Key', 'forms-bridge'),
+            'description' => __(
+                'You can get it from "SMTP & API" > "API Keys" page from your dashboard',
+                'forms-bridge'
+            ),
+            'type' => 'string',
+            'required' => true,
+        ],
+        [
+            'ref' => '#form/fields[]',
+            'name' => 'listIds',
+            'label' => __('List IDs', 'forms-bridge'),
+            'type' => 'string',
+            'description' => __(
+                'List IDs separated by commas. Leave it empty if you don\'t want to subscrive contact to any list',
+                'forms-bridge'
+            ),
+        ],
+    ],
+    'form' => [
+        'title' => __('Brevo Contacts', 'forms-bridge'),
+        'fields' => [
+            [
+                'name' => 'listIds',
+                'type' => 'hidden',
+                'required' => true,
+            ],
+            [
+                'name' => 'email',
+                'label' => __('Your email', 'forms-bridge'),
+                'type' => 'email',
+                'required' => true,
+            ],
+            [
+                'name' => 'fname',
+                'label' => __('Your firstname', 'forms-bridge'),
+                'type' => 'text',
+                'required' => false,
+            ],
+            [
+                'name' => 'lname',
+                'label' => __('Your lastname', 'forms-bridge'),
+                'type' => 'text',
+            ],
+        ],
+    ],
+    'backend' => [
+        'base_url' => 'https://api.brevo.com/v3/',
+        'headers' => [
+            [
+                'name' => 'Accept',
+                'value' => 'application/json',
+            ],
+        ],
+    ],
+    'hook' => [
+        'method' => 'POST',
+        'endpoint' => '/v3/contacts',
+        'pipes' => [],
+    ],
+];
---
+++ addons/rest-api/templates/mailchimp-contacts.php
@@ -0,0 +1,254 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit();
+}
+
+function forms_bridge_mailchimp_backend_headers($headers)
+{
+    if (isset($headers['api-key'])) {
+        $api_key = $headers['api-key'];
+        unset($headers['api-key']);
+
+        $headers['Authorization'] = 'Basic ' . base64_encode('key:' . $api_key);
+    }
+
+    remove_filter(
+        'http_bridge_backend_headers',
+        'forms_bridge_mailchimp_backend_headers',
+        10,
+        1
+    );
+
+    return $headers;
+}
+
+add_filter(
+    'forms_bridge_template_data',
+    function ($data, $template_name) {
+        if ($template_name === 'mailchimp-contacts') {
+            $index = array_search(
+                'datacenter',
+                array_column($data['fields'], 'name')
+            );
+
+            $dc = $data['fields'][$index]['value'];
+            $data['backend']['base_url'] = preg_replace(
+                '/\{dc\}/',
+                $dc,
+                $data['backend']['base_url']
+            );
+
+            $index = array_search(
+                'list_id',
+                array_column($data['fields'], 'name')
+            );
+
+            $list_id = $data['fields'][$index]['value'];
+            $data['hook']['endpoint'] = preg_replace(
+                '/\{list_id\}/',
+                $list_id,
+                $data['hook']['endpoint']
+            );
+        }
+
+        return $data;
+    },
+    10,
+    2
+);
+
+add_filter(
+    'forms_bridge_payload',
+    function ($payload, $hook) {
+        if ($hook->template !== 'mailchimp-contacts') {
+            return $payload;
+        }
+
+        $with_merge_fields = ['merge_fields' => []];
+
+        foreach ($payload as $field => $value) {
+            if ($field === 'email_address') {
+                $with_merge_fields[$field] = $value;
+            } elseif ($field === 'list_id' && $value) {
+                $with_merge_fields[$field] = strval((int) $value);
+            } elseif ($field === 'status') {
+                $with_merge_fields[$field] = in_array($value, [
+                    'subscribed',
+                    'unsubscribed',
+                    'cleaned',
+                    'pending',
+                    'transactional',
+                ])
+                    ? $value
+                    : 'pending';
+            } else {
+                $with_merge_fields['merge_fields'][strtoupper($field)] = $value;
+            }
+        }
+
+        $with_merge_fields['language'] = get_locale();
+
+        add_filter(
+            'http_bridge_backend_headers',
+            'forms_bridge_mailchimp_backend_headers',
+            10,
+            1
+        );
+
+        return $with_merge_fields;
+    },
+    9,
+    2
+);
+
+return [
+    'title' => __('MailChimp Contacts', 'forms-bridge'),
+    'fields' => [
+        [
+            'ref' => '#backend',
+            'name' => 'base_url',
+            'label' => __('MailChimp API URL', 'forms-bridge'),
+            'type' => 'string',
+            'value' => 'https://{dc}.api.mailchimp.com/3.0',
+        ],
+        [
+            'ref' => '#backend',
+            'name' => 'name',
+            'label' => __(
+                'Label of the MailChimp API backend connection',
+                'forms-bridge'
+            ),
+            'type' => 'string',
+            'default' => 'MailChimp API',
+        ],
+        [
+            'ref' => '#hook',
+            'name' => 'method',
+            'label' => __('Bridge HTTP method', 'forms-bridge'),
+            'type' => 'string',
+            'value' => 'POST',
+        ],
+        [
+            'ref' => '#hook',
+            'name' => 'endpoint',
+            'label' => __('Bridge endpoint', 'forms-bridge'),
+            'type' => 'string',
+            'value' => '/lists/{list_id}/members',
+        ],
+        [
+            'ref' => '#backend/headers[]',
+            'name' => 'api-key',
+            'label' => __('MailChimp API Key', 'forms-bridge'),
+            'description' => __(
+                'You can get it from "SMTP & API" > "API Keys" page from your dashboard',
+                'forms-bridge'
+            ),
+            'type' => 'string',
+            'required' => true,
+        ],
+        [
+            'ref' => '#form/fields[]',
+            'name' => 'datacenter',
+            'label' => __('Datacenter', 'forms-bridge'),
+            'type' => 'string',
+            'required' => true,
+        ],
+        [
+            'ref' => '#form/fields[]',
+            'name' => 'list_id',
+            'label' => __('Audience ID', 'forms-bridge'),
+            'description' => __(
+                'You can find the ID on the settings tab of your segments',
+                'forms-bridge'
+            ),
+            'type' => 'string',
+            'required' => true,
+        ],
+        [
+            'ref' => '#form/fields[]',
+            'name' => 'status',
+            'label' => __('Subscription status', 'forms-bridge'),
+            'type' => 'options',
+            'options' => [
+                [
+                    'label' => __('Subscribed', 'forms-bridge'),
+                    'value' => 'subscribed',
+                ],
+                [
+                    'label' => __('Unsubscribed', 'forms-bridge'),
+                    'value' => 'unsubscribed',
+                ],
+                [
+                    'label' => __('Pending', 'forms-bridge'),
+                    'value' => 'pending',
+                ],
+                [
+                    'label' => __('Cleaned', 'forms-bridge'),
+                    'value' => 'cleand',
+                ],
+                [
+                    'label' => __('Transactional', 'forms-bridge'),
+                    'value' => 'transactional',
+                ],
+            ],
+            'default' => 'subscribed',
+            'required' => true,
+        ],
+    ],
+    'form' => [
+        'title' => __('MailChimp Contacts', 'forms-bridge'),
+        'fields' => [
+            [
+                'name' => 'datacenter',
+                'type' => 'hidden',
+                'required' => true,
+            ],
+            [
+                'name' => 'list_id',
+                'type' => 'hidden',
+                'required' => true,
+            ],
+            [
+                'name' => 'status',
+                'type' => 'hidden',
+                'required' => true,
+            ],
+            [
+                'name' => 'email_address',
+                'label' => __('Your email', 'forms-bridge'),
+                'type' => 'email',
+                'required' => true,
+            ],
+            [
+                'name' => 'fname',
+                'label' => __('Your firstname', 'forms-bridge'),
+                'type' => 'text',
+            ],
+            [
+                'name' => 'lname',
+                'label' => __('Your lastname', 'forms-bridge'),
+                'type' => 'text',
+            ],
+        ],
+    ],
+    'backend' => [
+        'base_url' => 'https://{dc}.api.mailchimp.com/3.0',
+    ],
+    'hook' => [
+        'method' => 'POST',
+        'endpoint' => '/v3/contacts',
+        'pipes' => [
+            [
+                'from' => 'datacenter',
+                'to' => 'datacenter',
+                'cast' => 'null',
+            ],
+            [
+                'from' => 'list_id',
+                'to' => 'list_id',
+                'cast' => 'null',
+            ],
+        ],
+    ],
+];
