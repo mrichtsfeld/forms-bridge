82ea72d2cde937fde017b810747a6d9b13d8aa20
Lucas García <lucas@codeccoop.org>
2025-02-23 23:09:02 +0100

    fix: plugin version

--- forms-bridge.php
+++ forms-bridge.php
@@ -10,7 +10,7 @@
  * License URI:         http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:         forms-bridge
  * Domain Path:         /languages
- * Version:             3.0.1
+ * Version:             3.0.2
  * Requires PHP:        8.0
  * Requires at least:   6.7
  */
