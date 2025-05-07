2b94932aceb4fb1ad098a47d90d8f5b6104a0811
Lucas García <lucas@codeccoop.org>
2025-05-08 01:24:58 +0200

    feat: holded do_submit inherit from rest

--- addons/holded/class-holded-form-bridge.php
+++ addons/holded/class-holded-form-bridge.php
@@ -37,59 +37,6 @@ class Holded_Form_Bridge extends Rest_Form_Bridge
         return 'application/json';
     }
 
-    /**
-     * Performs an http request to backend's REST API.
-     *
-     * @param array $payload Payload data.
-     * @param array $attachments Submission's attached files.
-     *
-     * @return array|WP_Error Http request response.
-     */
-    protected function do_submit($payload, $attachments = [])
-    {
-        $response = parent::do_submit($payload, $attachments);
-
-        if (is_wp_error($response)) {
-            $error_response = $response->get_error_data()['response'];
-            if (
-                $error_response['response']['code'] !== 425 &&
-                $error_response['response']['code'] !== 400
-            ) {
-                return $response;
-            }
-
-            $data = json_decode($error_response['body'], true);
-            if ($data['code'] !== 'duplicate_parameter') {
-                return $response;
-            }
-
-            if (
-                !isset($payload['email']) ||
-                strstr($this->endpoint, '/v3/contacts') === false
-            ) {
-                return $response;
-            }
-
-            $update_response = $this->patch([
-                'name' => 'holded-update-contact-by-email',
-                'endpoint' => "/v3/contacts/{$payload['email']}?identifierType=email_id",
-                'method' => 'PUT',
-            ])->submit($payload);
-
-            if (is_wp_error($update_response)) {
-                return $update_response;
-            }
-
-            return $this->patch([
-                'name' => 'holded-search-contact-by-email',
-                'endpoint' => "/v3/contacts/{$payload['email']}",
-                'method' => 'GET',
-            ])->submit(['identifierType' => 'email_id']);
-        }
-
-        return $response;
-    }
-
     protected function api_schema()
     {
         $chunks = array_values(array_filter(explode('/', $this->endpoint)));
