bf8e8900724c6c70b43cf0fc819d785f217c13bc
Lucas García <lucas@codeccoop.org>
2025-06-25 23:18:18 +0200

    build(release): bump version to 3.5.4

--- forms-bridge.php
+++ forms-bridge.php
@@ -10,7 +10,7 @@
  * License URI:         http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:         forms-bridge
  * Domain Path:         /languages
- * Version:             3.5.3
+ * Version:             3.5.4
  * Requires PHP:        8.0
  * Requires at least:   6.7
  */
